f910af16008793df855a0b6f4be655ba0a460795
maisab <maaisa.barreto@gmail.com>
2015-12-11 09:44:15 -0200

    Nick MVC

--- campoMinado2.0/app/APP-7/CampoMinado/src/controller/NickNameControler.java
+++ campoMinado2.0/app/APP-7/CampoMinado/src/controller/NickNameControler.java
@@ -5,24 +5,35 @@
  */
 package controller;
 
+import javax.swing.JOptionPane;
+import model.Global;
 import model.Jogador;
+import model.OperacaoNick;
+import model.Time;
+import view.JDialogConTempo;
 
 /**
  *
  * @author maa
  */
 public class NickNameControler {
-    
+
     Jogador j;
+    OperacaoNick operacao = new OperacaoNick();
 
     public NickNameControler() {
         j = new Jogador();
     }
-    
-    public NickNameControler(Jogador j) {
-        this.j = j;
+    public void salvarNick(String nick) {
+
+        int tempo = JOptionPane.showConfirmDialog(null, "Deseja jogar com tempo limite ?!", "Tempo!", JOptionPane.YES_NO_OPTION);
+        if (tempo == JOptionPane.YES_OPTION) {
+            operacao.salvarNickTempo(j, nick);
+
+        } else {
+            operacao.salvarNick(j, nick);
+        }
+
     }
-    
-    
-    
+
 }
